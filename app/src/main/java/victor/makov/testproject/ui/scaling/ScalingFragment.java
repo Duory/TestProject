a86b4539b450a9997420c357d3f9eb5d7d610945
victor.makov <victor.makov@office.e2e4gu.ru>
2018-06-25 02:36:57 +0700

    cleanup

--- app/src/main/java/victor/makov/testproject/ui/scaling/ScalingFragment.java
+++ app/src/main/java/victor/makov/testproject/ui/scaling/ScalingFragment.java
@@ -2,10 +2,8 @@ package victor.makov.testproject.ui.scaling;
 
 import android.Manifest;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -15,12 +13,10 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
-import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageView;
 
 import java.io.File;
 import java.io.IOException;
@@ -29,7 +25,6 @@ import java.util.Date;
 
 import victor.makov.testproject.R;
 import victor.makov.testproject.di.Injectable;
-import victor.makov.testproject.util.PermissionUtils;
 
 public class ScalingFragment extends Fragment implements Injectable {
 
